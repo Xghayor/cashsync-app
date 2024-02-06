dcf67b1825772dbffc3533eda0f8d5c93e62597b
Xghayor <cc.baqir@gmail.com>
2024-02-06 20:19:57 +0100

    fix bugs and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  authenticated :user do
+    root 'users#show', as: :authenticated_root
+  end
+
   root to: "static_pages#home"
 
   resources :users do
